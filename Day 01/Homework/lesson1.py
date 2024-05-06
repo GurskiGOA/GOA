c239771bf20a973e94d5eb5b010fd8c0993c706c
GurskiGoa <gurski936@gmail.com>
2024-05-06 16:32:25 +0400

    first commit

---
+++ Day 01/Homework/lesson1.py
@@ -0,0 +1,33 @@
+# name = "Gurami"
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "Gurami" არის ცვლადისთვის მნიშვნელობა
+
+surname = "Tvaradze"
+
+# print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+ 
+name = "Gurami"  #ეს არის str (string) ტიპის ცვლადი
+age = 16 # ეს არის int (integer) მთელი რიცხვი 
+height = 182.3 # ეს არის floot ტიპის ცვლადი (ათწილადი)
+#booLean (booL) ტიპის ცვლადი
+
+knows_programminig = True #True  ან False
+is_ugly = False  #snakecase (უბრალოდ წერის სტილი სტანდარტულად )
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+# print(name + " " + surname)
+
+# სტრინგი არის ბრჭყალებშ მოქცეული სიმბოლოები
+# print(name + age) 
+
+#print(type (age))
+#print(type (name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programminig))
+
+print(name + " " + str(age))
